6b79afb14b1bc89545ccd6fabac7f9aa7ff19aaf
Hiroki Kato <hkato@sas.upenn.edu>
2018-05-08 17:13:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 def line(deli_line)
   if deli_line.length == 0
     puts "The line is currently empty."
+  else deli_line.each_with_index.map do |customer, number|
+    puts "The line is currently: #{number} #{customer}"
   end
 end
 
